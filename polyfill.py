622915b12dba6c9350ab9c7e6676a1ffbe0c3620
kennedn <kennedn@msn.com>
2020-07-06 18:43:20 +0100

    Restructure

--- polyfill.py
+++
@@ -1,8 +0,0 @@
-# Math.clamp implementation using min, max
-def clamp(min_x, max_x, x):
-    return max(min_x, min(max_x, x))
-
-
-# Enum helper, for 2.7 compatibility
-def enum(**enums):
-    return type('Enum', (), enums)
